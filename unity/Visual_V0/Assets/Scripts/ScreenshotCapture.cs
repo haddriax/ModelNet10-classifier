b123da7897a5604d7352e3c0068ea247881caef6
Fanny Badoules <fanny.badoules@student-cs.fr>
2026-02-19 17:51:37 +0100

    WIP add automatique mode to screen all models

--- unity/Visual_V0/Assets/Scripts/ScreenshotCapture.cs
+++ unity/Visual_V0/Assets/Scripts/ScreenshotCapture.cs
@@ -1,33 +1,64 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.IO;
+using System.Collections;
 
 public class ScreenshotCapture : MonoBehaviour
 {
     public GameObject cameraQ;
     public GameObject cameraP;
-    private GameObject[] objNames;    
 
-    void Update()
+    private static string OBJ_PATH = "Models/models_prefabs";
+
+    private GameObject[] modelsList;
+
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            TakeScreenshot(cameraQ);
-        }
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            TakeScreenshot(cameraP);
-        }
+        modelsList = Resources.LoadAll<GameObject>(OBJ_PATH);
+        StartCoroutine(PlaceObjectAndTakeScreenshots());
     }
 
-    void TakeScreenshot(GameObject camObj)
+
+    //void Update()
+    //{
+    //    Take screeshot manually
+    //    if (Input.GetKeyDown(KeyCode.Q))
+    //    {
+    //        TakeScreenshot(cameraQ);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.P))
+    //    {
+    //        TakeScreenshot(cameraP);
+    //    }
+    //}
+
+    IEnumerator PlaceObjectAndTakeScreenshots()
+    {        
+        foreach (GameObject model in modelsList)
+        {
+            Debug.Log("Start...");
+            GameObject obj = Instantiate(model, Vector3.zero, Quaternion.identity);
+
+            yield return new WaitForEndOfFrame();
+            TakeScreenshot(cameraQ, model.name);
+            Debug.Log("1st screenshot taken...");
+
+            yield return new WaitForEndOfFrame();
+            TakeScreenshot(cameraP, model.name);
+
+            Destroy(obj);
+            yield return null;
+        }
+        Debug.Log("End of the process");
+    }
+
+    void TakeScreenshot(GameObject camObj, string objName)
     {
         foreach (Camera cam in Camera.allCameras)
             cam.enabled = false;
-
-        objNames = GameObject.FindGameObjectsWithTag("Object");
         
-        string fileName = "Assets/ScreenShots" + camObj.name + "_" + objNames[0].name + ".png";
+        string fileName = "Assets/ScreenShots" + camObj.name + "_" + objName + ".png";
 
         if (camObj != null)
         {
